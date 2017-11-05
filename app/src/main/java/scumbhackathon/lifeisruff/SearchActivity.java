0a616f99ec7897b4c1e21f5e5c04057639c69dc5
JHthree <johnheino3@gmail.com>
2017-11-05 11:38:26 -0800

    get api

--- app/src/main/java/scumbhackathon/lifeisruff/SearchActivity.java
+++ app/src/main/java/scumbhackathon/lifeisruff/SearchActivity.java
@@ -4,13 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    String[] mobileArray = {"Ashes, \nGilroy, CA","Lucinda, \nSata Cruz, CA","Sheila, \nSalinas, CA","Dexter, \nFelton, CA",
+            "Fluffy the Destroyer, \nMonterey, CA","Rex, \nMarina, CA","Daisy Bell, \nSeaside, CA","Spot, \nMonterey, CA",
+            "nothing\narizona", "death \n everywhere"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.list_item, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 
     public void moveLostForm(View view) {
@@ -27,5 +47,12 @@ public class SearchActivity extends AppCompatActivity {
         Intent startNewActivity = new Intent(this, MapsActivity.class);
         startActivity(startNewActivity);
     }
+
+    // override other abstract methods here
+
+
+
+
+
 
 }
