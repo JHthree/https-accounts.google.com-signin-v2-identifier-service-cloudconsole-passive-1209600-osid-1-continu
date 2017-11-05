5008729e3eba7795d9f8cffe883957f9234e43a2
JHthree <johnheino3@gmail.com>
2017-11-04 18:01:45 -0700

    get api

--- app/src/main/java/scumbhackathon/lifeisruff/LostActivity.java
+++ app/src/main/java/scumbhackathon/lifeisruff/LostActivity.java
@@ -4,6 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LostActivity extends AppCompatActivity {
 
@@ -11,6 +29,10 @@ public class LostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost);
+
+
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
     }
 
     public void moveFoundForm(View view) {
@@ -27,4 +49,42 @@ public class LostActivity extends AppCompatActivity {
         Intent startNewActivity = new Intent(this, SearchActivity.class);
         startActivity(startNewActivity);
     }
+
+
+    private Spinner spinner1;
+    private Button btnSubmit;
+
+
+
+
+
+
+
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new scumbhackathon.lifeisruff.CustomOnItemSelectedListener());
+    }
+
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(LostActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) ,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
 }
+
+
